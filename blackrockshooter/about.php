609c3188b97b353e67d5e1e26f59ec1b5e48c4e5
Recry <34796160+recrys@users.noreply.github.com>
2017-12-24 00:21:06 +0700

    recry
    please use local server to develop

---
+++ blackrockshooter/about.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta name="Description" content="Basic web">
+	<meta name="author" content="Zidan">
+	<title>Basic web | about us</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div id="branding">
+				<h1><span class="highlight">Basic</span> Web Design</h1>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li class="current"><a href="about.php">About</a></li>
+					<li><a href="services.php">Services</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="find">
+		<div class="container">
+			<h1>Find what you need</h1>
+			<form>
+				<input type="text" placeholder="search here">
+				<button type="Submit" class="button1" value="Search">Search</button>
+			</form>	
+		</div>		
+	</section>
+
+	<section id="main">
+		<div class="container">
+			<article id="main-ar">
+
+				<h1 class="page-title">About Us</h1>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<br>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</article>
+
+			<aside id="sidebar">
+			<h3>Who is Black Rock Shooter?</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit</p>
+			</aside>
+		</div>
+	</section>
+
+	<footer>
+		<p>Basic Web Design &copy; Zidan 2017</p>
+	</footer>
+
+</body>
+</html>
---
+++ blackrockshooter/services.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta name="Description" content="Basic web">
+	<meta name="author" content="Zidan">
+	<title>Basic web | Home</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div id="branding">
+				<h1><span class="highlight">Basic</span> Web Design</h1>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About</a></li>
+					<li class="current"><a href="services.php">Services</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="find">
+		<div class="container">
+			<h1>Find what you need</h1>
+			<form>
+				<input type="text" placeholder="search here">
+				<button type="Submit" class="button1" value="Search">Search</button>
+			</form>	
+		</div>		
+	</section>
+
+	<section id="boxes">
+		<div class="container">
+			<div class="box">
+				<img src="img/brs1.png" alt="Synopsys">
+				<h3>Black Rock Shooter Synopsys</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+			<div class="box">
+				<img src="img/brs2.png" alt="Walpapper">
+				<h3>Black Rock Shooter Walpapper</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+			<div class="box">
+				<img src="img/brs3.png" alt="Character">
+				<h3>Black Rock Shooter Character</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<p>Basic Web Design &copy; Zidan 2017</p>
+	</footer>
+
+</body>
+</html>
